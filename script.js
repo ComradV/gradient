be5f5a9aa293bc1515b200d005839a3e2f474dde
Gleb Vasiliev <vasiliev.g.i@gmail.com>
2018-11-25 23:01:19 +0300

    Works for span-marks. Shows that marks' positions. Tested for 2 markers. Settings can be set in special object.

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ let precision = 0.01;
 // pdf returns the probability density function
 // It is a function of the number of sentences
 // away from central indicator.
-function pdf(s, precision) {
-    let k = Math.sqrt(precision / 2 / Math.PI)
-    let p = -precision * s * s / 2
-    return k * Math.exp(p);
-}
+
+// function pdf(s, precision) {
+//     let k = Math.sqrt(precision / 2 / Math.PI)
+//     let p = -precision * s * s / 2
+//     return k * Math.exp(p);
+// }
 
         // createTable returns a pdf table, where s ranges
         // from -L to L. resolution is the number of equally
@@ -65,51 +66,4 @@ function pmf(n, k) {
     return C(n, k) * Math.pow(p, n)
 }
 
-// createTable returns the pmf values where index 0 is
-// the marker-point. ls is the number of lines
-// in 1 direction that the gradient should
-// extend to.
-function createTable(ls) {
-
-    var range = 2 * ls;// + 1;
-
-    var out = {}
-
-    for (k = 0; k < range + 1; k++) {
-        let idx = -((range / 2) >> 0) + k
-        out[idx] = pmf(range, k)
-    }
-
-    // Scale values
-    highest = out[0]
-    Object.keys(out).forEach(function (key) {
-        out[key] = out[key] / highest;
-    });
-
-    return out;
-}
-
-function drawSquare(x, y, size) {
-    ctx.beginPath();
-    ctx.rect(x, y, size, size);
-    ctx.fill();
-    ctx.stroke();
-}
-
-var canvas = document.getElementById("canvas1");
-var ctx = canvas.getContext("2d");
-
-ctx.lineWidth = 0;
-
-
-let linesAbove = 10;
-let table = createTable(linesAbove)
-let keys = Object.keys(table).sort(function (a, b) { return a - b });
-
-for (k = 0; k < keys.length; k++) {
-    let val = table[keys[k]]
-    ctx.fillStyle = 'rgb(' + [255 * (1 - val), 255 * (1 - val), 255 * (1 - val)].join(',') + ')';
-    ctx.strokeStyle = ctx.fillStyle
-    drawSquare(0, 5 * k, 5);
-}
 
--- script2.js
+++ script2.js
@@ -1,56 +1,225 @@
-function getSentCoords() {
+let resolution = 50;
+let alpha = 1;
+let limit = 5;
+let markSettings = {
+    mark1: {
+        color: [0, 0, 0],
+        label: 'name1',
+        position: 1,
+    },
+    mark2: {
+        color: [255, 0, 0],
+        label: 'name2',
+        position: 2,
+    }
+};
+let scale = 1;
+
+
+function getMarkCoords() {
+    let paragraphs = document.getElementById('textElem').childNodes;
+
+    let marks = {}; //{mark1: [55,75], mark2:[70, 105]}
+
+    var range = document.createRange();
+    let top = document.getElementById('textElem').offsetTop;
+    for (let i = 0; i < paragraphs.length; i++) {
+        let paragraph = paragraphs[i];
+        if (paragraph.nodeName == '#text') {
+            continue;
+        }
+        let parChildren = paragraph.childNodes;
+        for (let j = 0; j < parChildren.length; j++) {
+            let node = parChildren[j];
+            if (node.nodeName == 'SPAN') {
+                range.selectNode(node);
+                var rects = range.getClientRects();
+                addMarks(node, (rects[0].top + rects[0].bottom) / 2 - top);
+            }
+        }
+    }
+
+    return marks;
+
+    function addMarks(node, coord) {
+        const classes = node.className.split(' ');
+        for (let i = 0; i < classes.length; i++) {
+            let markClass = classes[i];
+            if (marks[markClass] == undefined) {
+                marks[markClass] = []
+            }
+            marks[markClass].push(coord);
+        }
+    }
+
+}
+
+function onClickCoord(e) {
+    var marks = getMarkCoords();
+    redrawCanvas(marks);
+}
+
+function redrawCanvas(marks) {
+    // return;
+    let height = 100;
+    let width = 100;
+    // let height = document.getElementById('textElem').offsetHeight;
+    // let width = document.getElementById('canvas1').offsetWidth;
+    let squareSize = height / resolution;
+    let keys = Object.getOwnPropertyNames(marks);
+    for (let i = 0; i < keys.length; i++) {
+        let values = getSaturationValues(marks[keys[i]], squareSize, resolution)
+        var canvas = document.getElementById("canvas" + markSettings[keys[i]].position);
+        var ctx = canvas.getContext("2d");
+        ctx.lineWidth = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        let color = 'rgba(' +markSettings[keys[i]].color[0]+','+markSettings[keys[i]].color[1]+','+markSettings[keys[i]].color[2]+',';
+        for (let j = 0; j < resolution; j++) {
+            drawSquare(ctx, 0, squareSize * j, width*40, squareSize, color, values[j]);
+        }
+    }
+}
+
+function drawSquare(ctx, x, y, width, size, color, saturation) {
+    ctx.fillStyle = color + saturation * scale + ')';
+    // 'rgb(' + [255 * (1 - saturation), 255 * (1 - saturation), 255 * (1 - saturation)].join(',') + ')';
+    ctx.strokeStyle = ctx.fillStyle;
+    ctx.beginPath();
+    ctx.rect(x, y, width, size);
+    ctx.fill();
+    ctx.stroke();
+}
+
+function getSaturationValues(marks, squareSize, numSquares) {
+    let result = new Array(numSquares).fill(0);
+    for (let i = 0; i < marks.length; i++) {
+        putResult(result, Math.floor(marks[i] / squareSize), numSquares)
+    }
+    return result;
+}
+
+function putResult(result, pointToPut, numSquares) {
+    let profile = getProfile(pointToPut, numSquares);
+    for (let i = 0; i < result.length; i++) {
+        result[i] = Math.max(result[i], profile[i]);
+    }
+}
+
+function getProfile(point, numSquares) {
+    let result = new Array(numSquares).fill(0);
+    for (let i = 0; i < limit; i++) {
+        let val = Math.exp(-i * i / alpha)
+        let ind1 = point - i;
+        if (ind1 >= 0) { result[ind1] = val }
+        let ind2 = point + i;
+        if (ind2 < numSquares) { result[ind2] = val }
+    }
+    return result;
+}
+
+window.onresize = onClickCoord;
+onClickCoord();
+function drawBlue(){
+    let height = 100;
+    let width = 100;
+    var canvas = document.getElementById("canvas1");
+    var ctx = canvas.getContext("2d");
+    ctx.lineWidth = 0;
+    drawSquare(ctx,0,0,width,height,'rgba(0,0,255,',1);
+
+}
+document.getElementById('drawBlue').addEventListener('click',drawBlue);
+
+
+// createTable returns the pmf values where index 0 is
+// the marker-point. ls is the number of lines
+// in 1 direction that the gradient should
+// extend to.
+// function createTable(ls) {
+
+//     var range = 2 * ls;// + 1;
+
+//     var out = {}
+
+//     for (k = 0; k < range + 1; k++) {
+//         let idx = -((range / 2) >> 0) + k
+//         out[idx] = pmf(range, k)
+//     }
+
+//     // Scale values
+//     highest = out[0]
+//     Object.keys(out).forEach(function (key) {
+//         out[key] = out[key] / highest;
+//     });
+
+//     return out;
+// }
+
+
+// var canvas = document.getElementById("canvas1");
+// var ctx = canvas.getContext("2d");
+
+// ctx.lineWidth = 0;
+
+
+// let linesAbove = 5;
+// let table = createTable(linesAbove)
+// let keys = Object.keys(table).sort(function (a, b) { return a - b });
+
+// for (k = 0; k < keys.length; k++) {
+//     let val = table[keys[k]]
+
+//     drawSquare(0, 5 * k, 5, val);
+// }
+
+function getSentCoords1() {
     let paragraphs = document.getElementById('textElem').childNodes;
     const lines = [];
-    const sentences = [];
-    let marks = {};
+    // const sentences = [];
+    let marks = {}; //{mark1: [55,75], mark2:[70, 105]}
 
     for (let i = 0; i < paragraphs.length; i++) {
         let paragraph = paragraphs[i];
+        if (paragraph.nodeName == '#text') {
+            continue;
+        }
         var range = document.createRange();
+        var curPar = paragraph.firstChild;
         var sentences = paragraph.textContent.split('.'); //Find centers of sentences
         var start = 0;
         var end = 0;
-        for (var i = 0; i < sentences.length; i++) {
-            var sentence = sentences[i];
+        for (var j = 0; j < sentences.length; j++) {
+            var sentence = sentences[j];
             end = start + sentence.length;
-            range.setStart(paragraph, start);
-            range.setEnd(paragraph, end);
+            range.setStart(curPar, start);
+            range.setEnd(curPar, end - 1);
             var rects = range.getClientRects();
             start = end + 1;
         }
         var childs = paragraph.childNodes;
         let sentNumber = 0;
-        for(let k = 0; k < childs.length; k++){
-            if (childs[k].nodeName =='#text'){
+        for (let k = 0; k < childs.length; k++) {
+            if (childs[k].nodeName == '#text') {
                 let textContent = childs[k].textContent;
-                while(textContent.includes(sentences[sentNumber])){
+                while (textContent.includes(sentences[sentNumber])) {
                     sentNumber++;
                 }
-            } else if (childs[k].nodeName =='#span'){
+            } else if (childs[k].nodeName == '#span') {
                 addMarks(childs[k], sentNumber);
-                }
             }
         }
-        console.log(marks);
-    
+    }
+    console.log(marks);
 
-    function addMarks(span, sentNumber){
+
+    function addMarks(span, sentNumber) {
         const classes = span.className.split(' ');
-        for(let i = 0; i < classes.length; i++){
+        for (let i = 0; i < classes.length; i++) {
             let markClass = classes[i];
-            if(marks[markClass]==undefined){
+            if (marks[markClass] == undefined) {
                 marks[markClass] = []
             }
             marks[markClass].push(sentNumber);
         }
     }
 }
-
-
-function onClickCoord(e) {
-    var coords = getSentCoords();
-    console.log(coords);
-
-}
-
-document.getElementById('info').addEventListener('click', onClickCoord);
