1ece2013a271bcf78b66db72c9e96656b99c4fee
Gleb Vasiliev <vasiliev.g.i@gmail.com>
2018-12-04 14:18:43 +0300

    Added sentence detection system. Marks are added in mid of the sentence.

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-let resolution = 50;
+let resolution = 20;
 let alpha = 1;
 let limit = 5;
 let markSettings = {
@@ -17,40 +17,78 @@ let scale = 1;
 
 
 function getMarkCoords() {
-    let paragraphs = document.getElementById('textElem').childNodes;
+    // let paragraphs = document.getElementById('textElem').childNodes;
 
     let marks = {}; //{mark1: [55,75], mark2:[70, 105]}
 
+    let textElement = document.getElementById('textElem');
     var range = document.createRange();
-    let top = document.getElementById('textElem').offsetTop;
+    let top = textElement.offsetTop;
+    let paragraphs = textElement.children;
+    let classes = [];
     for (let i = 0; i < paragraphs.length; i++) {
         let paragraph = paragraphs[i];
-        if (paragraph.nodeName == '#text') {
-            continue;
-        }
         let parChildren = paragraph.childNodes;
+        let startNotSet = true;
+        let start = 0, end = 0;
         for (let j = 0; j < parChildren.length; j++) {
             let node = parChildren[j];
             if (node.nodeName == 'SPAN') {
-                range.selectNode(node);
-                var rects = range.getClientRects();
-                addMarks(node, (rects[0].top + rects[0].bottom) / 2 - top);
+                // var rects = range.getClientRects();
+                classes.push(...getMarks(node));
+                // addMarks(node, (rects[0].top + rects[0].bottom) / 2 - top);
+            } else { //it is text node
+                if (startNotSet) {
+                    startNotSet = false;
+                    range.setStart(node, 0);
+                }
+                let firstDot = node.textContent.indexOf('.');
+                let lastDot = node.textContent.lastIndexOf('.');
+                if (firstDot >= 0 && classes.length > 0) {
+                    range.setEnd(node, firstDot);
+                    let rects = range.getClientRects();
+                    start = rects[0].top;
+                    end = rects[rects.length - 1].bottom;
+                    addMarksToCoord(classes, (start + end) / 2 - top);
+                }
+                if (lastDot >= 0) {
+                    if (node.textContent.length - 1 == lastDot) {
+                        startNotSet = true;
+                    } else {
+                        range.setStart(node, lastDot+1);
+                    }
+                    classes = [];
+                }
             }
         }
     }
 
     return marks;
 
-    function addMarks(node, coord) {
-        const classes = node.className.split(' ');
-        for (let i = 0; i < classes.length; i++) {
-            let markClass = classes[i];
+    function getMarks(node) {
+        return node.className.split(' ');
+    }
+
+    function addMarksToCoord(marksArray, coord) {
+        for (let i = 0; i < marksArray.length; i++) {
+            let markClass = marksArray[i];
             if (marks[markClass] == undefined) {
                 marks[markClass] = []
             }
             marks[markClass].push(coord);
         }
     }
+
+    // function addMarks(node, coord) {
+    //     const classes = node.className.split(' ');
+    //     for (let i = 0; i < classes.length; i++) {
+    //         let markClass = classes[i];
+    //         if (marks[markClass] == undefined) {
+    //             marks[markClass] = []
+    //         }
+    //         marks[markClass].push(coord);
+    //     }
+    // }
 
 }
 
@@ -73,9 +111,9 @@ function redrawCanvas(marks) {
         var ctx = canvas.getContext("2d");
         ctx.lineWidth = 0;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        let color = 'rgba(' +markSettings[keys[i]].color[0]+','+markSettings[keys[i]].color[1]+','+markSettings[keys[i]].color[2]+',';
+        let color = 'rgba(' + markSettings[keys[i]].color[0] + ',' + markSettings[keys[i]].color[1] + ',' + markSettings[keys[i]].color[2] + ',';
         for (let j = 0; j < resolution; j++) {
-            drawSquare(ctx, 0, squareSize * j, width*40, squareSize, color, values[j]);
+            drawSquare(ctx, 0, squareSize * j, width * 40, squareSize, color, values[j]);
         }
     }
 }
@@ -120,16 +158,16 @@ function getProfile(point) {
 
 window.onresize = onClickCoord;
 onClickCoord();
-function drawBlue(){
+function drawBlue() {
     let height = 100;
     let width = 100;
     var canvas = document.getElementById("canvas1");
     var ctx = canvas.getContext("2d");
     ctx.lineWidth = 0;
-    drawSquare(ctx,0,0,width,height,'rgba(0,0,255,',1);
+    drawSquare(ctx, 0, 0, width, height, 'rgba(0,0,255,', 1);
 
 }
-document.getElementById('drawBlue').addEventListener('click',drawBlue);
+// document.getElementById('drawBlue').addEventListener('click', drawBlue);
 
 
 // createTable returns the pmf values where index 0 is
@@ -173,54 +211,54 @@ document.getElementById('drawBlue').addEventListener('click',drawBlue);
 //     drawSquare(0, 5 * k, 5, val);
 // }
 
-function getSentCoords1() {
-    let paragraphs = document.getElementById('textElem').childNodes;
-    const lines = [];
-    // const sentences = [];
-    let marks = {}; //{mark1: [55,75], mark2:[70, 105]}
+// function getSentCoords1() {
+//     let paragraphs = document.getElementById('textElem').childNodes;
+//     const lines = [];
+//     // const sentences = [];
+//     let marks = {}; //{mark1: [55,75], mark2:[70, 105]}
 
-    for (let i = 0; i < paragraphs.length; i++) {
-        let paragraph = paragraphs[i];
-        if (paragraph.nodeName == '#text') {
-            continue;
-        }
-        var range = document.createRange();
-        var curPar = paragraph.firstChild;
-        var sentences = paragraph.textContent.split('.'); //Find centers of sentences
-        var start = 0;
-        var end = 0;
-        for (var j = 0; j < sentences.length; j++) {
-            var sentence = sentences[j];
-            end = start + sentence.length;
-            range.setStart(curPar, start);
-            range.setEnd(curPar, end - 1);
-            var rects = range.getClientRects();
-            start = end + 1;
-        }
-        var childs = paragraph.childNodes;
-        let sentNumber = 0;
-        for (let k = 0; k < childs.length; k++) {
-            if (childs[k].nodeName == '#text') {
-                let textContent = childs[k].textContent;
-                while (textContent.includes(sentences[sentNumber])) {
-                    sentNumber++;
-                }
-            } else if (childs[k].nodeName == '#span') {
-                addMarks(childs[k], sentNumber);
-            }
-        }
-    }
-    console.log(marks);
+//     for (let i = 0; i < paragraphs.length; i++) {
+//         let paragraph = paragraphs[i];
+//         if (paragraph.nodeName == '#text') {
+//             continue;
+//         }
+//         var range = document.createRange();
+//         var curPar = paragraph.firstChild;
+//         var sentences = paragraph.textContent.split('.'); //Find centers of sentences
+//         var start = 0;
+//         var end = 0;
+//         for (var j = 0; j < sentences.length; j++) {
+//             var sentence = sentences[j];
+//             end = start + sentence.length;
+//             range.setStart(curPar, start);
+//             range.setEnd(curPar, end - 1);
+//             var rects = range.getClientRects();
+//             start = end + 1;
+//         }
+//         var childs = paragraph.childNodes;
+//         let sentNumber = 0;
+//         for (let k = 0; k < childs.length; k++) {
+//             if (childs[k].nodeName == '#text') {
+//                 let textContent = childs[k].textContent;
+//                 while (textContent.includes(sentences[sentNumber])) {
+//                     sentNumber++;
+//                 }
+//             } else if (childs[k].nodeName == '#span') {
+//                 addMarks(childs[k], sentNumber);
+//             }
+//         }
+//     }
+//     console.log(marks);
 
 
-    function addMarks(span, sentNumber) {
-        const classes = span.className.split(' ');
-        for (let i = 0; i < classes.length; i++) {
-            let markClass = classes[i];
-            if (marks[markClass] == undefined) {
-                marks[markClass] = []
-            }
-            marks[markClass].push(sentNumber);
-        }
-    }
-}
+//     function addMarks(span, sentNumber) {
+//         const classes = span.className.split(' ');
+//         for (let i = 0; i < classes.length; i++) {
+//             let markClass = classes[i];
+//             if (marks[markClass] == undefined) {
+//                 marks[markClass] = []
+//             }
+//             marks[markClass].push(sentNumber);
+//         }
+//     }
+// }
